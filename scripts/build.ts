d6a5aa1cf781065ee70042398344c7b3e831d7a9
Justin Bennett <oss@just-be.dev>
2022-03-24 20:44:43 -0400

    Move build to vite; test to vitest; upgrade deps

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,28 +1,34 @@
 import path from "node:path";
-import url from "node:url";
-import fs from "node:fs/promises";
 
-import tsup from "tsup";
+import { build, LibraryFormats } from "vite";
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const fileName = (format: string) => `index.${format === "es" ? "js" : "cjs"}`;
+const formats: LibraryFormats[] = ["es", "cjs"];
 
-const dist = path.join(__dirname, "..", "dist");
-
-await fs
-  .access(dist)
-  .then(() => fs.rm(dist, { recursive: true }))
-  .catch(() => {});
-
-await tsup.build({
-  entryPoints: ["lib/node/index.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
-  outDir: "dist/node",
+await build({
+  build: {
+    outDir: "dist/node",
+    lib: {
+      entry: path.resolve(process.cwd(), "lib/node/index.ts"),
+      formats,
+      fileName,
+    },
+    rollupOptions: {
+      external: [/@remix-run\/dev\/.+/g, "vite"],
+    },
+  },
 });
 
-await tsup.build({
-  entryPoints: ["lib/client/index.ts"],
-  format: ["esm"],
-  dts: true,
-  outDir: "dist/client",
+await build({
+  build: {
+    outDir: "dist/client",
+    lib: {
+      entry: path.resolve(process.cwd(), "lib/client/index.ts"),
+      formats,
+      fileName,
+    },
+    rollupOptions: {
+      external: ["react", "react-router-dom"],
+    },
+  },
 });
--- tests/get-routes.test.ts
+++ tests/get-routes.test.ts
@@ -1,11 +1,9 @@
 import path from "node:path";
 import url from "node:url";
-import { test } from "uvu";
-import * as assert from "uvu/assert";
 
 import routesArrayFixture from "./fixtures/routes-array.js";
 
-import { getRoutes } from "../dist/node";
+import { getRoutes } from "../lib/node";
 
 const dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const appDir = path.join(dirname, "../examples/basic/src");
@@ -14,7 +12,5 @@ test("getRoutes", async () => {
   const routes = getRoutes({ appDir });
   const actual = JSON.stringify(routes, null, 4);
 
-  assert.fixture(actual, routesArrayFixture);
+  expect(actual).toEqual(routesArrayFixture);
 });
-
-test.run();
