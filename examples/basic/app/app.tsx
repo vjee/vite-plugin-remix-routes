90690d5707ec5ade9a4f6b60e598a7fa5aadb301
vjee <vjee2e@gmail.com>
2022-03-29 20:53:26 +0200

    Use local version of package in example, remove EagerLoader usage

--- examples/basic/app/app.tsx
+++ examples/basic/app/app.tsx
@@ -1,15 +1,8 @@
 import { useRoutes } from "react-router-dom";
-import { EagerLoader } from "vite-plugin-remix-routes/client";
 import routes from "virtual:remix-routes";
 
 export default function App() {
   const element = useRoutes(routes);
 
-  return (
-    <>
-      {/* More info about EagerLoader in the README */}
-      <EagerLoader routes={routes} />
-      {element}
-    </>
-  );
+  return <>{element}</>;
 }
